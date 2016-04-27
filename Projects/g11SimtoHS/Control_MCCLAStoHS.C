cc3ffdbe33c70668637d2298ec6ac6688e940e3b
dglazier <Derek.Glazier@glasgow.ac.uk>
2016-04-27 22:45:50 +0100

    Proof support for MCCLAStoHS

--- Projects/g11SimtoHS/Control_MCCLAStoHS.C
+++ Projects/g11SimtoHS/Control_MCCLAStoHS.C
@@ -8,17 +8,23 @@
    
   //Set up the input chain to be analysed
    TChain* tree=new TChain("h10","datachain");
-   tree->Add("/run/media/dglazier/hd_dglazier/Data/g11_pipi3/g11_pipi32001*.bos.evt.recsis.root");
+   tree->Add("YOURINPUTFILES");
  
    //Set the ouput directory or file (if full file name is given just 1 file is created, while if a directory it will be created and filled with files of the same name as the input 
    gSystem->Setenv("HSOUT",
-		   "/run/media/dglazier/hd_dglazier/Data/HSANAtest/");
+		   "YOUROUTPUTDIRECTORYORFILE");
  
    //Configure the analysis (i.e PROOF, source compilation...)
    TString HSANA=gSystem->Getenv("HSANA");
    gROOT->LoadMacro(HSANA+"/HSControl.C");
    TProof* prf=HSControl(kFALSE,kFALSE); //where the proof stuff is set, kTRUE means use PROOF
-   
+   if(prf){
+     //prf->Load(TString(gSystem->Getenv("ROOTSYS"))+"/lib/libPhysics.so");
+     TString ELOSS=gSystem->Getenv("ELOSS");
+     prf->Load(ELOSS+"/libeloss.so");
+     prf->AddIncludePath(ELOSS);
+   }	
+
    //Use an event list from a previous analysis 
    //If only 1 file was output the event list will be in its top directory, so just give the file name
    //If many files were ouput the event list will be in the same directory with the name ParentEventList.root
